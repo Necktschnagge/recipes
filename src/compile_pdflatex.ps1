2ede337bba11b1c939266a2e1b087c2c211d3bc4
Maximilian Starke <maximilian.starke@medionmail.com>
2020-03-18 10:49:27 +0100

    equalize log output

--- src/compile_pdflatex.ps1
+++ src/compile_pdflatex.ps1
@@ -2,6 +2,8 @@ pdflatex.exe -synctex=1 -interaction=nonstopmode .\book.tex > custom_log.log
 Get-Content custom_log.log | findstr /r "at( )*line[s]*[0-9]*" > custom_errors.log
 Get-Content custom_errors.log
 if ((Get-Content custom_errors.log).length -ne 0){
-	Write-Error "There were errors during compilation."
+	Write-Error "There were errors when compiling with latex!"
 	exit 1;
+	} else {
+	Write-Output "No errors or warnings found by regex."
 	}
