93c99cd72a4af4995352e5bda79e4358a87cb907
Maximilian Starke <maximilian.starke@medionmail.com>
2020-09-14 10:14:02 +0200

    test auth

--- script/conditional_deploy_artifact.sh
+++ script/conditional_deploy_artifact.sh
@@ -7,6 +7,6 @@ ls -la
 cd $HOME/	
 ls -la
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then # it is a pull request build.
-echo "This is a pull request"
+echo "This is a pull request ${GITHUB_TOKEN}"
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Hello world\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
