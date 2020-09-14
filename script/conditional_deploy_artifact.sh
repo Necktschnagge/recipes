9a7e483397bad71c35f0f5c1a00a5f4ee99ea558
Maximilian Starke <maximilian.starke@medionmail.com>
2020-09-14 09:23:22 +0200

    add semi-complete deploy script replacing lines in travis.yml

---
+++ script/conditional_deploy_artifact.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mv book.pdf "$(git rev-parse HEAD).pdf"
+git status
+ls -la
+cd $TRAVIS_BUILD_DIR/
+ls -la
+cd $HOME/	
+ls -la
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then # it is a pull request build.
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Hello world\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
