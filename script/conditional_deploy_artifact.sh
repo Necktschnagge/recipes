401228ba84140969de870f7b98d7076e1e59a149
Maximilian Starke <maximilian.starke@medionmail.com>
2020-09-16 15:36:09 +0200

    self-review

--- script/conditional_deploy_artifact.sh
+++ script/conditional_deploy_artifact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then # it is a pull request build.
-echo "This is a pull request build: https://github.com/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}"
-echo "Check labels of pull request ${TRAVIS_PULL_REQUEST}"
+echo "This is a pull request build: https://github.com/${TRAVIS_REPO_SLUG}/pull/${TRAVIS_PULL_REQUEST}"
+echo "Check labels of pull request ${TRAVIS_PULL_REQUEST}:"
 labels=$(curl -H "Authorization: token ${GH_TRAVIS_TOKEN}" -X GET "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/labels")
 echo $labels
 [[ $labels =~ ^.*2352840377.*$ ]] && echo "Found ID (2352840377) of label >disable-preview<. Abort preview deployment." && exit 0
@@ -29,7 +29,7 @@ git push https://${TRAVIS_USERNAME}:${TRAVIS_PASSWORD}@github.com/Necktschnagge/
 echo "Uploading pdf build artifact... DONE"
 popd
 if [[ $labels =~ ^.*2352896968.*$ ]] ; then
-echo "Found ID (2352896968) of label >disable-preview<. Skip posting a comment to the pull request."
+echo "Found ID (2352896968) of label >skip-preview-link<. Skip posting a comment to the pull request linking to the preview PDF."
 exit 0
 fi
 echo "Posting comment into pull request..."
