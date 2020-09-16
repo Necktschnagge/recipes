95ecd40d8ce7accca0306d9bd4b3e094150fdf21
Maximilian Starke <maximilian.starke@medionmail.com>
2020-09-16 14:03:33 +0200

    enhanced logging, abort on disable-preview label

--- script/conditional_deploy_artifact.sh
+++ script/conditional_deploy_artifact.sh
@@ -3,7 +3,8 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then # it is a pull request build.
 echo "This is a pull request build: ...repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}"
 echo "Check labels of pull request ${TRAVIS_PULL_REQUEST}"
 labels=$(curl -H "Authorization: token ${GH_TRAVIS_TOKEN}" -X GET "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/labels")
-[[ $labels =~ ^.*2352840377.*$ ]] && echo "match with regex found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo $labels
+[[ $labels =~ ^.*2352840377.*$ ]] && echo "Found ID (2352840377) of label >disable-preview<. Abort preview deployment" && exit 0
 echo "Start uploading pdf build artifact..."
 pushd .
 git_hash=$(git rev-parse HEAD)
@@ -14,15 +15,16 @@ git clone "https://github.com/${TRAVIS_REPO_SLUG}.git" .
 git checkout artifacts
 cd artifacts
 mv $TRAVIS_BUILD_DIR/src/book.pdf "${git_hash}.pdf"
-ls -la
-cat .git/config
 git status
+echo ">>>>> git add -f ./${git_hash}.pdf"
 git add -f "./${git_hash}.pdf"
-git status
+echo ">>>>> git commit -m \"Automatic upload of preview pdf\""
 git commit -m "Automatic upload of preview pdf"
 git status
 git push https://${TRAVIS_USERNAME}:${TRAVIS_PASSWORD}@github.com/Necktschnagge/recipes HEAD
+echo "Uploading pdf build artifact... DONE"
 popd
+echo "Posting comment into pull request"
 curl -H "Authorization: token ${GH_TRAVIS_TOKEN}" -X POST -d "{\"body\": \"[See build preview here: ${git_hash}.pdf](https://github.com/Necktschnagge/recipes/blob/artifacts/artifacts/${git_hash}.pdf)\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 else 
 echo "This is no pull request build. Skipping artifact upload."
