47534ec5e8ba227ab023014159c4a17f782bbd50
Maximilian Starke <maximilian.starke@medionmail.com>
2020-09-16 13:41:52 +0200

    try fixing

--- script/conditional_deploy_artifact.sh
+++ script/conditional_deploy_artifact.sh
@@ -19,7 +19,7 @@ git commit -m "Automatic upload of preview pdf"
 git status
 git push https://${TRAVIS_USERNAME}:${TRAVIS_PASSWORD}@github.com/Necktschnagge/recipes HEAD
 popd
-curl -H "Authorization: token ${GH_TRAVIS_TOKEN}" -X GET -d "{\"body\": \"[See build preview here: ${git_hash}.pdf](https://github.com/Necktschnagge/recipes/blob/artifacts/art"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/labels"
+curl -H "Authorization: token ${GH_TRAVIS_TOKEN}" -X GET "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/labels"
 
 curl -H "Authorization: token ${GH_TRAVIS_TOKEN}" -X POST -d "{\"body\": \"[See build preview here: ${git_hash}.pdf](https://github.com/Necktschnagge/recipes/blob/artifacts/artifacts/${git_hash}.pdf)\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 else 
